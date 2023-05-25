305e309f90aa9b120fe605ef686a0cd57c64a676
nikhilareddyputta <nikhila.putta.reddy@gmail.com>
2023-05-25 08:34:58 +0530

    Updates

---
+++ precache-manifest.f0e892d4bdee195b373c05268e9a8995.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaaa7bb3c0a73d521ddf",
+    "url": "/static/css/main.d63ebd20.chunk.css"
+  },
+  {
+    "revision": "aaaa7bb3c0a73d521ddf",
+    "url": "/static/js/main.aaaa7bb3.chunk.js"
+  },
+  {
+    "revision": "50d14688add0fc79ec49",
+    "url": "/static/css/1.673f8637.chunk.css"
+  },
+  {
+    "revision": "50d14688add0fc79ec49",
+    "url": "/static/js/1.50d14688.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "331ba90f5c8f946aa63c6120efac8bdc",
+    "url": "/static/media/logo3.331ba90f.svg"
+  },
+  {
+    "revision": "2d96e98bfb1c393a5bd3d5b9bedb65ab",
+    "url": "/static/media/Group 71.2d96e98b.svg"
+  },
+  {
+    "revision": "ee679dac7d69fb52141f5ff845c99c3d",
+    "url": "/static/media/Plans Expenses 1.ee679dac.svg"
+  },
+  {
+    "revision": "f1289f186f0c876b88f103b4a1264d63",
+    "url": "/static/media/My Profile - Experiences 2.f1289f18.svg"
+  },
+  {
+    "revision": "a6fff9ca4d3e21f769a7101f68a90497",
+    "url": "/static/media/Place Page - Itinerary 1.a6fff9ca.svg"
+  },
+  {
+    "revision": "cecc4305c70b129c0f19c9556a5cc5c4",
+    "url": "/static/media/Add Experience 2 (1) 1.cecc4305.svg"
+  },
+  {
+    "revision": "96222572d7074ce5f92de04de51ae7b9",
+    "url": "/static/media/Nomads Moto.96222572.svg"
+  },
+  {
+    "revision": "d46f3da464f75e509ce1b675cc47fd30",
+    "url": "/static/media/Nomads Road Trip.d46f3da4.svg"
+  },
+  {
+    "revision": "9a64af9a020affff5efd3de247c585bf",
+    "url": "/static/media/Group 32951.9a64af9a.svg"
+  },
+  {
+    "revision": "454a46232786462161f2e50fe586e685",
+    "url": "/index.html"
+  }
+];
